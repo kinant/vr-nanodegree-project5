8a811066129bea8d7db23140f335eacdbbafc45b
Kinan Turman <kinan.t@gmail.com>
2017-02-07 10:48:11 -0600

    fixed menu item positions. added temporary items for other options

--- VR Space Exploration Exhibit/Assets/Scripts/VRInteractiveItem.cs
+++ VR Space Exploration Exhibit/Assets/Scripts/VRInteractiveItem.cs
@@ -9,13 +9,27 @@ public class VRInteractiveItem : MonoBehaviour, IGvrGazeResponder {
 	public event Action OnExit;
 	public event Action OnTrigger;
 
+	protected bool m_IsOver;
+
+	public bool IsOver {
+		get {
+			return m_IsOver;
+		}
+	}
+
 	// Implement IGvrGazeResponder
 	public void OnGazeEnter(){
+
+		m_IsOver = true;
+
 		if (OnEnter != null)
 			OnEnter ();
 	}
 
 	public void OnGazeExit(){
+
+		m_IsOver = false;
+
 		if (OnExit != null)
 			OnExit ();
 	}
