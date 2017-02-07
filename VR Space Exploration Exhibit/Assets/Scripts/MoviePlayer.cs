efbf7f28a8dd6ab5306ed147a35d377fe9450a2c
Kinan Turman <kinan.t@gmail.com>
2017-02-07 04:59:21 -0600

    added videos. one works with the movie player and the other with animations

---
+++ VR Space Exploration Exhibit/Assets/Scripts/MoviePlayer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoviePlayer : MonoBehaviour {
+
+    [SerializeField]
+    private VRInteractiveItem m_VRInteractiveItem; // The VRInteractiveItem that needs to be looked at for the textures to play.
+
+    [SerializeField]
+    private MediaPlayerCtrl m_player;
+
+    private void OnEnable()
+    {
+        m_VRInteractiveItem.OnEnter += HandleOver;
+        m_VRInteractiveItem.OnExit += HandleOut;
+    }
+
+
+    private void OnDisable()
+    {
+        m_VRInteractiveItem.OnEnter -= HandleOver;
+        m_VRInteractiveItem.OnExit -= HandleOut;
+    }
+
+    private void HandleOver() {
+        m_player.Play();
+    }
+
+    private void HandleOut() {
+        m_player.Stop();
+    }
+
+
+}
