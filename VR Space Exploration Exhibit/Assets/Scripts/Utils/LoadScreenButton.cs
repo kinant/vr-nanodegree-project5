df18be9cd80c144a70063a76b3ab4436cccb0c9f
Kinan Turman <kinan.t@gmail.com>
2017-02-07 11:38:21 -0600

    added basic loading screen

---
+++ VR Space Exploration Exhibit/Assets/Scripts/Utils/LoadScreenButton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScreenButton : MonoBehaviour {
+	[SerializeField]
+	private VRInteractiveItem m_VRInteractiveItem;
+
+	public int num_scene_load;
+
+	void OnEnable(){
+		m_VRInteractiveItem.OnTrigger += LoadScreen;
+	}
+
+	void OnDisable(){
+		m_VRInteractiveItem.OnTrigger -= LoadScreen;
+	}
+
+
+	private void LoadScreen(){
+		if (num_scene_load < 0 || num_scene_load >= SceneManager.sceneCountInBuildSettings) {
+			return;
+		}
+		LoadingScreenManager.LoadScene (num_scene_load);
+	}
+}
