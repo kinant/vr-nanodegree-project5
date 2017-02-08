d0a3166c824d48995c50d7cc9a8c5dde3ce0e8ff
Kinan Turman <kinan.t@gmail.com>
2017-02-08 15:38:18 -0600

    created rover and rover control using the trigger on certain parts of the ground. rover uses a nav mesh agent to move around

--- VR Space Exploration Exhibit/Assets/TapSurface.cs
+++ VR Space Exploration Exhibit/Assets/TapSurface.cs
@@ -1,16 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class TapSurface : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+	[SerializeField]
+	private VRInteractiveItem m_VRInteractiveItem;
+
+	[SerializeField]
+	private NavMeshAgent roverNavMeshAgent;
+
+	public LayerMask surfaceLayer;
+	private RaycastHit hit;
+
+	void OnEnable(){
+		m_VRInteractiveItem.OnTrigger += OnScreenTap;
+	}
+
+	void OnDisable(){
+		m_VRInteractiveItem.OnTrigger -= OnScreenTap;
+	}
+
+
+	public void OnPointerEnter(){
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	public void OnScreenTap(){
+		if (Physics.Raycast (Camera.main.transform.position, Camera.main.transform.forward, out hit, 100, surfaceLayer)) {
+			Debug.Log ("raycast hit!");
+			Debug.Log ("Target hit: " + hit.collider.tag);
+			Debug.Log ("hit position: " + hit.point);
+			roverNavMeshAgent.destination = hit.point;
+		}
 	}
 }
