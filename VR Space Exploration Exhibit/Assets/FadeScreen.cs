e2a6d3927373a224e5b355ef2ac969a77ff27f6d
Kinan Turman <kinan.t@gmail.com>
2017-02-15 10:55:12 -0600

    added fade-in script and a fade in to all the scenes

---
+++ VR Space Exploration Exhibit/Assets/FadeScreen.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeScreen : MonoBehaviour {
+
+	public Image fadeOverlay;
+	public float fadeSpeed = 1.5f;
+	public bool sceneStarting = true;
+
+	void Update(){
+		if (sceneStarting) {
+			StartScene ();
+		}
+	}
+
+	void FadeToClear(){
+		fadeOverlay.color = Color.Lerp (fadeOverlay.color, Color.clear, fadeSpeed * Time.deltaTime);
+	}
+
+	void StartScene(){
+		FadeToClear ();
+
+		if (fadeOverlay.color.a <= 0.05f) {
+
+			fadeOverlay.color = Color.clear;
+			fadeOverlay.enabled = false;
+
+			sceneStarting = false;
+		}
+	}
+}
